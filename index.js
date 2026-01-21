68a08c5b2bd483c706ed460d1e748a22521960c8
Sim <eodb45@gmail.com>
2026-01-21 22:07:19 +0900

    Wls수정

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ function openModal(index) {
   }
 }
 
-    function closeModal() {
-      document.getElementById("myModal").style.display = "none";
-    }
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
 
 window.onclick = function (event) {
   const modal = document.getElementById("myModal");
